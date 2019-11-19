c04f0c6e815624aaa603f2df21cbe6a384c1d5f9
gregjhansell97 <gregjhansell@gmail.com>
2019-11-19 17:26:16 -0500

    wip

---
+++ tests/test_transformations.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import pytest
+from text_transformation.transformer import title, strip, ngrams, Transformer
+
+__author__ = "gregjhansell97"
+__copyright__ = "gregjhansell97"
+__license__ = "mit"
+
+
+def test_transformer_title():
+    t = Transformer("title")
+    pass
+
+def test_transformer_stripped():
+    t = Transformer("stripped")
+    pass
+
+def test_transformer_ngrams():
+    t = Transformer("ngrams")
+    pass
+
+def test_title():
+    pass
