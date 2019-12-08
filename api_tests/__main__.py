0be503b20c94acf68d831c2c058325fca8c941e7
Jeff Yang <www.speedway@hotmail.com>
2019-12-08 16:09:10 -0500

    api tests update & takes one command line arg. as port#

--- api_tests/__main__.py
+++ api_tests/__main__.py
@@ -13,17 +13,16 @@ def test1():
     
     # the json that goes with the request
     data = {
-        "$id" : 1,
-        "$type": "html",
-        "$data": "<p>hello hello world this is some test, go and parse this!<\p>",
-        "$transformations": [
-            "stripped",
-            "1-gram",
-            "2-gram",
-            "3-gram",
-            "title",
-        ],
+        "type": "html",
+        "data": "<p>hello hello world this is some test, go and parse this!<\p>",
+        "transformations": {
+            "stripped": True,
+            "grams": [1,2,3],
+            "Title": False
+        }
     }
+    
+    data = json.dumps(json)
     
     # making the request
     respond = requests.put(addr, json=data)
@@ -36,30 +35,33 @@ def test1():
         respond_json["stripped"]
         == "hello hello world this is some test go and parse this"
     ), "stripped words do not match (test 1)"
-
-    words = {
-        "$hello": [0, 1],
-        "$world": [2],
-        "$some": [5],
-        "$test": [6],
-        "$go": [7],
-        "$parse": [9],
+    
+    grams = {
+        "1":{
+            "hello": [0, 1],
+            "world": [2],
+            "some": [5],
+            "test": [6],
+            "go": [7],
+            "parse": [9]
+        },
+        "2":{
+            "hello hello": [0],
+            "hello world": [1],
+            "some test": [5],
+            "test go": [6]
+        },
+        "3":{
+            "hello hello world": [0],
+            "some test go": [5]
+        }
     }
-    assert respond_json["1-gram"] == words, "words do not match (test 1)"
-
-    bigrams = {
-        "$hello hello": [0],
-        "$hello world": [1],
-        "$some test": [5],
-        "$test go": [6],
-    }
-    assert respond_json["2-gram"] == bigrams, "bigrams do not match (test 1)"
-
-    trigrams = {
-        "$hello hello world": [0],
-        "$some test go": [5],
-    }
-    assert respond_json["3-gram"] == trigrams, "trigrams go not match (test 1)"
+    
+    grams = json.dumps(grams)
+    
+    assert respond_json["grams"]["1"] == grams["1"], "words do not match (test 1)"
+    assert respond_json["grams"]["2"] == grams["2"], "bigrams do not match (test 1)"
+    assert respond_json["grams"]["3"] == grams["3"], "trigrams go not match (test 1)"
 
 
 def test2():
@@ -73,17 +75,16 @@ def test2():
     
     # the json that goes with the request
     data = {
-        "$id" : 1,
-        "$type": "html",
-        "$data": "<p>hello hello world this is some test, go and parse this!<\p>",
-        "$transformations": [
-            "invalid-param",
-            "1-gram",
-            "2-gram",
-            "3-gram",
-            "title",
-        ],
+        "type": "html",
+        "data": "<p>hello hello world this is some test, go and parse this!<\p>",
+        "transformations": {
+            "stripped": True,
+            "grams": [1,2,3],
+            "Title": False
+        }
     }
+    
+    data = json.dumps(json)
     
     respond = requests.put(addr, json=data)
     
@@ -101,17 +102,16 @@ def test3():
     
     # the json that goes with the request
     data = {
-        "$id" : 1,
-        "$type": "html",
-        "$data": "",
-        "$transformations": [
-            "stripped",
-            "1-gram",
-            "2-gram",
-            "3-gram",
-            "title",
-        ],
+        "type": "html",
+        "data": "",
+        "transformations": {
+            "stripped": True,
+            "grams": [1,2,3],
+            "Title": False
+        }
     }
+    
+    data = json.dumps(data)
     
     # making the request
     respond = requests.put(addr, json=data)
@@ -119,17 +119,15 @@ def test3():
     # converting data we received in response to json
     respond_json = respond.json()
     
+    
     # make sure we're getting the correct response
     assert respond_json["stripped"] == ""
 
-    words = {}
-    assert respond_json["1-gram"] == words, "words do not match (test 1)"
+    assert respond_json["grams"]["1"] == {}, "words do not match (test 1)"
 
-    bigrams = {}
-    assert respond_json["2-gram"] == bigrams, "bigrams do not match (test 1)"
+    assert respond_json["grams"]["2"] == {}, "bigrams do not match (test 1)"
 
-    trigrams = {}
-    assert respond_json["3-gram"] == trigrams, "trigrams go not match (test 1)"
+    assert respond_json["grams"]["3"] == {}, "trigrams go not match (test 1)"
 
 
 def test4():
@@ -182,11 +180,11 @@ def test_submitty_html():
     # we've ran the data through hw2 so the statistics should be correct
     assert len(respond_json["stripped"]) == 539, "total word count incorrect"
     
-    assert len(respond_json["1-gram"]) == 309, "unique word count incorrect"
+    assert len(respond_json["grams"]["1"]) == 309, "unique word count incorrect"
     
-    assert len(respond_json["2-gram"]) == 369, "unique bigram count incorrect"
+    assert len(respond_json["grams"]["2"]) == 369, "unique bigram count incorrect"
     
-    assert len(respond_json["3-gram"]) == 341, "unique trigram count incorrect"
+    assert len(respond_json["grams"]["3"]) == 341, "unique trigram count incorrect"
     
 def test_ecse_1010_html():
     print("testing with ECSE_1010.json")
@@ -207,11 +205,11 @@ def test_ecse_1010_html():
     # we've ran the data through hw2 so the statistics should be correct
     assert len(respond_json["stripped"]) == 3603, "total word count incorrect"
     
-    assert len(respond_json["1-gram"]) == 908, "unique word count incorrect"
+    assert len(respond_json["grams"]["1"]) == 908, "unique word count incorrect"
     
-    assert len(respond_json["2-gram"]) == 1487, "unique bigram count incorrect"
+    assert len(respond_json["grams"]["2"]) == 1487, "unique bigram count incorrect"
     
-    assert len(respond_json["3-gram"]) == 1373, "unique trigram count incorrect"
+    assert len(respond_json["grams"]["3"]) == 1373, "unique trigram count incorrect"
     
 def test_wikipedia_html():
     print("testing with wikipedia.json")
@@ -232,11 +230,11 @@ def test_wikipedia_html():
     # we've ran the data through hw2 so the statistics should be correct
     assert len(respond_json["stripped"]) == 34070, "total word count incorrect"
     
-    assert len(respond_json["1-gram"]) == 5695, "unique word count incorrect"
+    assert len(respond_json["grams"]["1"]) == 5695, "unique word count incorrect"
     
-    assert len(respond_json["2-gram"]) == 12118, "unique bigram count incorrect"
+    assert len(respond_json["grams"]["2"]) == 12118, "unique bigram count incorrect"
     
-    assert len(respond_json["3-gram"]) == 12617, "unique trigram count incorrect"
+    assert len(respond_json["grams"]["3"]) == 12617, "unique trigram count incorrect"
     
 def test_grpc_html():
     print("testing with grpc.json")
@@ -257,14 +255,14 @@ def test_grpc_html():
     # we've ran the data through hw2 so the statistics should be correct
     assert len(respond_json["stripped"]) == 7091, "total word count incorrect"
     
-    assert len(respond_json["1-gram"]) == 715, "unique word count incorrect"
+    assert len(respond_json["grams"]["1"]) == 715, "unique word count incorrect"
     
-    assert len(respond_json["2-gram"]) == 1543, "unique bigram count incorrect"
+    assert len(respond_json["grams"]["2"]) == 1543, "unique bigram count incorrect"
     
-    assert len(respond_json["3-gram"]) == 1497, "unique trigram count incorrect"
+    assert len(respond_json["grams"]["3"]) == 1497, "unique trigram count incorrect"
 
 def test_diodes_html():
-    print("testing with grpc.json")
+    print("testing with diodes.json")
     
     # the port number will be replaced with the actual port number that the
     # text transformation server is using
@@ -282,11 +280,11 @@ def test_diodes_html():
     # we've ran the data through hw2 so the statistics should be correct
     assert len(respond_json["stripped"]) == 21740, "total word count incorrect"
     
-    assert len(respond_json["1-gram"]) == 2099, "unique word count incorrect"
+    assert len(respond_json["grams"]["1"]) == 2099, "unique word count incorrect"
     
-    assert len(respond_json["2-gram"]) == 4778, "unique bigram count incorrect"
+    assert len(respond_json["grams"]["2"]) == 4778, "unique bigram count incorrect"
     
-    assert len(respond_json["3-gram"]) == 6945, "unique trigram count incorrect"
+    assert len(respond_json["grams"]["3"]) == 6945, "unique trigram count incorrect"
 
 def test_non_json_put_request():
     print("testing with non-json data")
@@ -299,7 +297,7 @@ def test_non_json_put_request():
     # so we should be receiving some error code from the server since this
     # request doesn't make sense
     text = "this is some random text and this request doesn't make any sense"
-    
+
     # make the request
     respond = requests.put(addr, data = text)
     
